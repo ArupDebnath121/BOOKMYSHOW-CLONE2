a4d9c7f567bb65fc0ccd784169b612ae3af7f411
Arup Debnath <105730107+ArupDebnath121@users.noreply.github.com>
2022-12-10 23:53:37 +0530

    Completed movie navbar and movie info sections

--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -7,6 +7,7 @@ import { MovieContext } from "../context/Movie.comtext";
 import Slider from "react-slick";
 import { FaCcVisa, FaCcApplePay } from "react-icons/fa";
 import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+import MovieHero from "../components/MovieHero/MovieHero.Component";
 
 const MoviePage = () => {
   const { id } = useParams();
@@ -44,22 +45,53 @@ const MoviePage = () => {
     requestRecommendedMovies();
   }, [id]);
 
-  //   useEffect(() => {
-  //     const requestMovie = async () => {
-  //       const getMovieData = await axios.get(`/movie/${id}`);
-  //       setMovie(getMovieData.data);
-  //     };
+  useEffect(() => {
+    const requestMovie = async () => {
+      const getMovieData = await axios.get(`/movie/${id}`);
+      setMovie(getMovieData.data);
+    };
 
-  //     requestMovie();
-  //   }, [id]);
+    requestMovie();
+  }, [id]);
 
   const settingCast = {};
 
-  const settings = {};
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          initialSlide: 4,
+        },
+      },
+    ],
+  };
 
   return (
     <>
-      {/* <MovieHero /> */}
+      <MovieHero />
       <div className="my-12 container px-4 lg-ml-20 lg:w-2/3">
         <div className="flex flex-col items-start gap-3">
           <h1 className="text-gray-800 font-bold text-2xl">About the movie</h1>
